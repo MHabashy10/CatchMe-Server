63f8fb52518e80b3efe3e0440f8eae9ca6856b63
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-09-03 17:56:24 +0200

    no need to allow CORS since we use ionic proxy

--- src/app.ts
+++ src/app.ts
@@ -26,26 +26,26 @@ class App {
     this.express.use(bodyParser.urlencoded({ extended: false }));
 
     // Add headers
-    this.express.use(function (req, res, next) {
+    // this.express.use(function (req, res, next) {
 
-      // Website you wish to allow to connect
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //   // Website you wish to allow to connect
+    //   res.setHeader('Access-Control-Allow-Origin', '*');
+    //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
 
-      // Request methods you wish to allow
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //   // Request methods you wish to allow
+    //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-      // Request headers you wish to allow
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    //   // Request headers you wish to allow
+    //   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-      // Set to true if you need the website to include cookies in the requests sent
-      // to the API (e.g. in case you use sessions)
-      res.setHeader('Access-Control-Allow-Credentials', 'true');
+    //   // Set to true if you need the website to include cookies in the requests sent
+    //   // to the API (e.g. in case you use sessions)
+    //   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-      // Pass to next layer of middleware
-      next();
-    });
+    //   // Pass to next layer of middleware
+    //   next();
+    // });
 
   }
 
