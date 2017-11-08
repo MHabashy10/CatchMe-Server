df87241cbf670e1751e35f8525579a4b12cda7ab
Mohamed Habshey <Mohamed.habshey10@gmail.com>
2017-11-08 16:47:42 +0200

    add pre save hook :)

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -4,6 +4,8 @@ import { Document, Schema, Model, model } from 'mongoose';
 
 
 export var userSchema: Schema = new Schema({
+    createdAt: Date,
+    updatedAt: Date,
     firstName: String,
     lastName: String,
     email: String,
@@ -11,4 +13,15 @@ export var userSchema: Schema = new Schema({
     phone: Number,
     avatar: String
 });
+
+userSchema.pre("save", function (next) {
+    let now = new Date();
+    if (!this.createdAt) {
+        this.createdAt = now;
+    } else {
+        this.updatedAt = now;
+    }
+    next();
+});
+
 export const User: Model<any> = model('User', userSchema);
