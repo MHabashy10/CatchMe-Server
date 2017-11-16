983604676e3157b3aaeb7116fd8dfffe92ed8abb
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-11-16 23:49:15 +0200

    correct condition if no email OR no pass

--- src/routes/AccountRouter.ts
+++ src/routes/AccountRouter.ts
@@ -60,7 +60,7 @@ export class AccountRouter {
     let email = req.body.email;
     let password = req.body.password;
     let hero = true //Heroes.find(hero => hero.id === query);
-    if (!(email || password)) {
+    if (!(email && password)) {
       return res.send({ status: false })
     }
 
